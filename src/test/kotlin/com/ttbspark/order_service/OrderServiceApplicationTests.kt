0ed9252da36b46084d0aa1cf9bf27eeee4f79c7e
armary12 <armarygg@gmail.com>
2025-02-28 18:41:24 +0700

    fist commit

---
+++ src/test/kotlin/com/ttbspark/order_service/OrderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ttbspark.order_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
