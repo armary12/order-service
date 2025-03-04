8ad1db5dcde488d4672a967224825c89189e97e3
armary12 <armarygg@gmail.com>
2025-03-04 18:18:13 +0700

    feat: add security config

---
+++ src/main/kotlin/com/ttbspark/order/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.ttbspark.order.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@Profile("local")
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests { auth ->
+                auth.anyRequest().permitAll()
+            }
+        return http.build()
+    }
+}
